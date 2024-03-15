92e0ad157901ae8f1b911eccd4580ea42fff6e22
The-GO0d-Guy <malaha814@gmail.com>
2024-03-15 19:51:20 +0530

    home-page-completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,13 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-[#B1D7D0] h-screen w-screen flex flex-col">
+    <div className="bg-[#E1F9F5] h-screen w-screen flex flex-col">
       <Navbar />
       <div className="text-[#00856F] mx-10 flex gap-6 relative top-32 left-[75%] font-semibold">
         <span className="hover:text-[#559c90]">Consult a doctor</span>
         <span className="hover:text-[#559c90]">Get Medical Tests</span>
       </div>
-      <div className="w-[35%] text-3xl lg:text-4xl text-[#00856F] relative top-[35%] left-[15%] font-semibold">
+      <div className="w-[40%] text-3xl lg:text-4xl text-[#00856F] relative top-[27%] left-[15%] font-semibold">
         <span>
           "Empowering you to thrive in wellness, because a healthy life is a
           journey worth embracing."
@@ -23,9 +23,15 @@ export default function Home() {
         />
         <img
           src="home/generate.svg"
-          className="w-[30%] absolute bottom-28 left-[15%] hover:shadow-xl"
+          className="w-[30%] absolute bottom-32 left-[15%] hover:shadow-xl"
           alt="generate"
         />
+        <button className="w-[10%] absolute bottom-14 left-[15%] p-3 text-[#00856F] font-semibold bg-[#B1D7D0] hover:bg-[#99d7cb] rounded-[10px]">
+          Scan Report
+        </button>
+        <button className="w-[10%] absolute bottom-14 left-[30%] p-3 text-[#00856F] font-semibold bg-[#B1D7D0] hover:bg-[#99d7cb] rounded-[10px]">
+          Lab Test
+        </button>
       </div>
     </div>
   );
